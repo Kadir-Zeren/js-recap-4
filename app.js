90be51a5948fbb77c0edbd15ffbddf5584f12dd8
kadir-zeren <k4dzeren@gmail.com>
2023-11-21 03:40:18 +0300

    v3

--- app.js
+++ app.js
@@ -17,17 +17,27 @@
 //   const randomNumber = Math.round(Math.random() * 100);
 //   console.log(`${sayac}. your number's : ${randomNumber}`);
 // }
-let i = 1;
-while (i < 10) {
-  console.log(i);
-  i++;
-}
-console.log("bye");
+// let i = 1;
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+// console.log("bye");
+
+// let not = prompt("lütfen 0-100 arasinda bir not giriniz");
+
+// while (not < 0 || not > 100) {
+//     console.log("Not 0'dan küçük veya 100'den büyük olamaz");
+//     not = prompt("lütfen 0-100 arasinda bir not giriniz");
+// }
+// console.log("Notunuz:", not);
 
 let not = prompt("lütfen 0-100 arasinda bir not giriniz");
 
-while (not < 0 || not > 100) {
-  console.log("Not 0'dan küçük veya 100'den büyük olamaz");
+do {
   not = prompt("lütfen 0-100 arasinda bir not giriniz");
-}
+  if (not < 0 || not > 100) {
+    alert("Not 0'dan kücük veya 100'den büyük olamaz ");
+  }
+} while (not < 0 || not > 100);
 console.log("Notunuz:", not);
