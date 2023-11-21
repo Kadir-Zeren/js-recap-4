5c93231059bccd9d9e91f1d1279687b3fed1a10a
kadir-zeren <k4dzeren@gmail.com>
2023-11-21 03:33:15 +0300

    v2

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
-for (let i = 0; i <= 100; i++) {
-  console.log(`${i}-Hello Js`);
+// for (let i = 0; i <= 100; i++) {
+//   console.log(`${i}-Hello Js`);
+// }
+
+// const n = prompt("enter your number");
+// let sum = 0;
+
+// for (let i = 1; i <= n; i++) {
+//   sum += i;
+//   console.log(sum);
+// }
+// console.log(`SUM: ${sum}`);
+
+// const number = prompt("How many number:?");
+
+// for (let sayac = 1; sayac <= number; sayac++) {
+//   const randomNumber = Math.round(Math.random() * 100);
+//   console.log(`${sayac}. your number's : ${randomNumber}`);
+// }
+let i = 1;
+while (i < 10) {
+  console.log(i);
+  i++;
 }
+console.log("bye");
 
-const n = prompt("enter your number");
-let sum = 0;
+let not = prompt("lütfen 0-100 arasinda bir not giriniz");
 
-for (let i = 1; i <= n; i++) {
-  sum += i;
-  console.log(sum);
+while (not < 0 || not > 100) {
+  console.log("Not 0'dan küçük veya 100'den büyük olamaz");
+  not = prompt("lütfen 0-100 arasinda bir not giriniz");
 }
-console.log(`SUM: ${sum}`);
-
-const number = prompt("How many number:?");
-
-for (let sayac = 1; sayac <= number; sayac++) {
-  const randomNumber = Math.round(Math.random() * 100);
-  console.log(`${sayac}. your number's : ${randomNumber}`);
-}
+console.log("Notunuz:", not);
